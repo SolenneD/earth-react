e1d14201a293ee9575e98b39f9f7a4a02987185a
Solenne DAGUERRE <sodag@P-ASN-HOENN.dhcp.idf.intranet>
2019-03-08 10:28:43 +0100

    problem OBJLoader

--- src/components/Satellites.js
+++ src/components/Satellites.js
@@ -1,30 +1,23 @@
-/********** Imports **********/
 import React, { PureComponent } from 'react';
 import * as THREE from 'three';
+import OBJLoader from 'three-obj-loader';
+OBJLoader('THREE');
 
 export class Satellites extends PureComponent {
 
   componentDidMount() {
     // Satellite Sphere
-    // this.geometry = new THREE.SphereGeometry( this.props.data.r, 32, 32 );
     this.geometry = new THREE.SphereGeometry( 10, 32, 32 );
-    this.material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-    this.sphere = new THREE.Mesh( this.geometry, this.material );
-    this.sphere.position.set(50, 50, 50);
-    this.props.scene.add( this.sphere );
+    // Texture
+    this.texture = new THREE.OBJLoader().load
+    ('textures/Satellite.obj',
+      texture => {
+        this.sat = new THREE.MeshBasicMaterial( { map: this.texture } );
+        this.sphere = new THREE.Mesh( this.geometry, this.sat );
+        this.props.scene.add( this.sphere );
+      }
+    )
   }
-    
-
-
-
-  // componentDidUpdate() {
-  //   // update satelite pos.
-  //   const radius = this.props.data.r;
-  //   const scale = radius * 1;
-  //   this.sphere.scale.x = scale;
-  //   this.sphere.scale.y = scale;
-  //   this.sphere.scale.z = scale;
-  // }
 
   render() {
     return null;
