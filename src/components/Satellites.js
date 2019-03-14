71971e00edb5f5948d53fd33829806c1af15c36a
Solenne DAGUERRE <sodag@P-ASN-HOENN.dhcp.idf.intranet>
2019-03-14 16:21:16 +0100

    click sat v1

--- src/components/Satellites.js
+++ src/components/Satellites.js
@@ -1,157 +1,127 @@
 /********** Imports **********/
-import React, {
-  PureComponent
-} from 'react';
-import * as THREE from 'three';
-
-import satData from '../data/sat.json';
-
+import React, { PureComponent } from 'react'
+import * as THREE from 'three'
+ 
+import satData from '../data/sat.json'
+ 
+const cardStyle = {
+  zIndex: 999,
+  width: '100%',
+  background: 'white',
+  position: "absolute",
+  opacity: .5
+}
+ 
 export class Satellites extends PureComponent {
-
   constructor(props) {
     super(props)
-
-    this.state = {
-      open: true
-    }
+    this.state = { open: true, isCardOpen: false, selectedSatData: {} }
     // this.openModal = this.openModal.bind(this);
     // this.closeModal = this.closeModal.bind(this);
-    this.x = {};
-    this.mouse = {};
-    this.raycaster = new THREE.Raycaster();
-    this.mouse = new THREE.Vector2();
-    this.cardShown = false;
-    this.clickedObj = {};
-    this.renderer = new THREE.WebGLRenderer();
-
-
+    this.x = {}
+    this.mouse = {}
+    this.raycaster = new THREE.Raycaster()
+    this.mouse = new THREE.Vector2()
   }
-
-
-  showCard = (userText) => {
-    this.divElement = document.getElementsByName(".card");
-
-    if (this.divElement) {
-      if (!this.cardShown) {
-        this.divElement.css({
-          display: "none",
-        });
-      }
-
-      this.divElement.text("Object color: " + userText);
-
-      if (!this.cardShown) {
-        setTimeout(function () {
-          this.divElement.css({
-            display: "block",
-          });
-        }, 25);
-      }
-
-      this.cardShown = true;
+ 
+  onDocumentMouseDown = event => {
+    event.preventDefault()
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+    // find intersections
+    this.raycaster.setFromCamera(this.mouse, this.props.camera)
+    this.intersects = this.raycaster.intersectObjects(
+      this.props.scene.children,
+      true
+    )
+    if (this.intersects.length > 0) {
+      const clickedSphere = this.intersects[0].object
+      if (clickedSphere.callback) clickedSphere.callback()
+ 
+      this.intersects[0].object.material.color.setHex(Math.random() * 0xffffff)
+ 
+      this.particle = new THREE.Sprite(this.particleMaterial)
+      this.particle.position.copy(this.intersects[0].point)
+ 
+      this.particle.scale.x = this.particle.scale.y = 16
+      this.props.scene.add(this.particle)
     }
   }
-
-  hideCard = () => {
-    this.divElement = document.getElementsByName(".card");
-    if (this.divElement) {
-      this.divElement.css({
-        display: "block",
-      });
-      this.cardShown = false;
-    }
-  }
-
-  updateMouseCoords = (event, coordsObj) => {
-    coordsObj.x = ((event.clientX - this.renderer.domElement.offsetLeft + 0.5) / window.innerWidth) * 2 - 1;
-    coordsObj.y = -((event.clientY - this.renderer.domElement.offsetTop + 0.5) / window.innerHeight) * 2 + 1;
-  }
-
-  onMouseUp = (event) => {
-    this.updateMouseCoords(event, this.mouse);
-
-    this.latestMouseProjection = undefined;
-    this.clickedObj = undefined;
-
-    this.raycaster.setFromCamera(this.mouse, this.camera); {
-      this.intersects = this.raycaster.intersectObjects(this.clickableObjects);
-
-      if (this.intersects.length > 0) {
-        this.latestMouseProjection = this.intersects[0].point;
-        this.clickedObj = this.intersects[0].object;
-        this.showCard(this.clickedObj.userData.userText);
-      } else {
-        this.clickedObj = undefined;
-        this.hideCard();
-      }
-    }
-  }
-
-
+ 
   // openModal (){
   //   this.setState({ open: false })
   // }
-
+ 
   // closeModal () {
   //   this.setState({ open: true })
   // }
-
+ 
   componentDidMount() {
     // Satellite Sphere
     // this.geometry = new THREE.SphereGeometry( this.props.data.r, 32, 32 );
     this.geometry = new THREE.SphereGeometry(10, 32, 32)
-    this.material = new THREE.MeshBasicMaterial({
-      color: 0xffff00
-    })
+    this.material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
     this.sphere = new THREE.Mesh(this.geometry, this.material)
-    this.sphere.callback = function () {
-      console.log('Toto!')
+    this.sphere.callback = () => {
+      // affiche id 1 (iss)
+      this.showCard(1)
     }
     this.sphere.position.set(50, 50, 50)
     this.props.scene.add(this.sphere)
-    window.addEventListener('mouseup', this.onMouseUp, false);
+    document.addEventListener('mousedown', this.onDocumentMouseDown, false)
   }
-
-  // componentDidUpdate() {
-  //   // update satelite pos.
-  //   const radius = 10;
-  //   const scale = radius * 1;
-  //   this.sphere.scale.x = scale;
-  //   this.sphere.scale.y = scale;
-  //   this.sphere.scale.z = scale;
-  // }
-
-
+ 
+  showCard(selectedSatId) {
+    // Recupère un  satellite dans le sat.json où l'id est égale à celui passé en paramètre (voir plus haut)
+    const selectedSatData = satData.filter(sat => sat.id === selectedSatId)[0]
+ 
+    // Met à jour la card
+    if (!this.state.isCardOpen) {
+      this.setState({ isCardOpen: true, selectedSatData })
+    } else {
+      this.setState({ isCardOpen: false, selectedSatData: {} })
+    }
+  }
+ 
+  componentDidUpdate() {
+    // update satelite pos.
+    const radius = 1
+    const scale = radius * 1
+    this.sphere.scale.x = scale
+    this.sphere.scale.y = scale
+    this.sphere.scale.z = scale
+  }
+ 
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.onDocumentMouseDown)
+  }
+ 
   render() {
     return (
       <div>
-        {satData.map((satDetail, index) => {
-          return (
-            <div key={index} className='card'>
-              <h2>{satDetail.satName.toUpperCase()}</h2>
-              <div className='cardImg'>
-                <img src={satDetail.satImg} alt={satDetail.satAlt} />
-              </div>
-              <div>
-                <p>
-                  Altitude : <span>{satDetail.alt}</span> km
-                </p>
-                <p>
-                  Longitude : <span>{satDetail.long}</span> °
-                </p>
-                <p>
-                  Latitude : <span>{satDetail.lat}</span> °
-                </p>
-              </div>
-              <button onClick={this.closeModal}>Fermer</button>
+        {this.state.isCardOpen ? (
+          <div className='card'>
+            <h2>{this.state.selectedSatData.satName.toUpperCase()}</h2>
+            <div className='cardImg'>
+              <img
+                src={this.state.selectedSatData.satImg}
+                alt={this.state.selectedSatData.satAlt}
+              />
             </div>
-          )
-        })}
-        {/* {this.state.isCardOpen ? (
-          <div style={cardStyle}>
-            <p>{this.state.currentCardData.name}</p>
+            <div>
+              <p>
+                Altitude : <span>{this.state.selectedSatData.alt}</span> km
+              </p>
+              <p>
+                Longitude : <span>{this.state.selectedSatData.long}</span> °
+              </p>
+              <p>
+                Latitude : <span>{this.state.selectedSatData.lat}</span> °
+              </p>
+            </div>
+            <button onClick={() => this.showCard()}>Fermer</button>
           </div>
-        ) : null} */}
+        ) : null}
       </div>
     )
   }
